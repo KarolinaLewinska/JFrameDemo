cb9508effb3aa6c6321b9cbe0aced62495a88e23
KarolinaLewinska <lewinskak95@gmail.com>
2021-08-14 15:22:53 +0200

    add JWindow & JInternalFrame

---
+++ src/InternalFrame.java
@@ -0,0 +1,29 @@
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+
+public class InternalFrame extends JFrame {
+    JDesktopPane desktopPane = new JDesktopPane();
+    JInternalFrame intFrame = new JInternalFrame("JInternalFrame demo");
+
+    public InternalFrame() {
+        intFrame.setMaximizable(true);
+        intFrame.setIconifiable(true);
+        intFrame.setResizable(true);
+        intFrame.setClosable(true);
+        intFrame.setDefaultCloseOperation(JInternalFrame.DISPOSE_ON_CLOSE);
+        intFrame.setSize(320, 240);
+        intFrame.setVisible(true);
+
+        desktopPane.add(intFrame);
+        add(desktopPane);
+    }
+
+    public static void main(String[] args) {
+        InternalFrame iFrame = new InternalFrame();
+        iFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        iFrame.setSize(640, 480);
+        iFrame.setVisible(true);
+
+    }
+}
