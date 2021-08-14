592846cef95b44ebe26d5b1a26a2d428a83890d7
KarolinaLewinska <lewinskak95@gmail.com>
2021-08-14 14:06:04 +0200

    Add panel to frame

--- src/Frame.java
+++ src/Frame.java
@@ -3,6 +3,8 @@ import javax.swing.*;
 public class Frame {
     public static void main(String[] args) {
         JFrame frame = new JFrame("JFrame demo");
+        Panel panel = new Panel();
+        frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setSize(320, 240);
         frame.setVisible(true);
---
+++ src/Panel.java
@@ -0,0 +1,25 @@
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Panel extends JPanel {
+    BufferedImage image;
+
+    public Panel() {
+        try {
+            image = ImageIO.read(new File("C:/Users/lewin/Desktop/summer.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        g.drawImage(image, 0, 0, null);
+        g.setColor(Color.CYAN);
+        g.fillRect(10, 10, 100, 50);
+    }
+}
