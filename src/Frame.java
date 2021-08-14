b5ac198ce880ea08c8b4d150310c9fd227f4323b
KarolinaLewinska <lewinskak95@gmail.com>
2021-08-14 13:13:20 +0200

    add JFrame

---
+++ src/Frame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Frame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("JFrame demo");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(320, 240);
+        frame.setVisible(true);
+
+    }
+}
