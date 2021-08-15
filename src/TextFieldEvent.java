91b31c587ec7d15553862236b708fc239203a09b
KarolinaLewinska <lewinskak95@gmail.com>
2021-08-15 08:29:20 +0200

    Add TextFieldEvent class

---
+++ src/TextFieldEvent.java
@@ -0,0 +1,64 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+public class TextFieldEvent {
+
+    JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+    JTextField textField = new JTextField("", 20);
+    JButton button = new JButton("Ok");
+
+    public TextFieldEvent() {
+        textField.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                textField.setText("");
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                textField.setText("Please enter some text...");
+            }
+        });
+
+        textField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                warn();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                warn();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                warn();
+            }
+
+            public void warn() {
+                button.setText(textField.getText());
+            }
+        });
+
+
+        panel.add(textField);
+        panel.add(button);
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(TextFieldEvent::new);
+    }
+}
