7e7b85fb367a4a3d2f9b52eaac83efeca6b59cfb
KarolinaLewinska <lewinskak95@gmail.com>
2021-08-15 08:03:51 +0200

    Add ButtonEvent class

---
+++ src/com/event/ButtonEvent.java
@@ -0,0 +1,24 @@
+package com.event;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import java.awt.Color;
+
+public class ButtonEvent {
+
+    JFrame frame = new JFrame("Button event demo");
+    JButton button = new JButton("Click me");
+
+    public ButtonEvent() {
+        button.addActionListener(e -> button.setBackground(Color.RED));
+        frame.add(button);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(ButtonEvent::new);
+    }
+}
